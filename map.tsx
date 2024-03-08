0d5099c98ac0304346eb0644acc044aae6aca259
Your Name <you@example.com>
2024-03-08 09:21:54 +0100

    animation et deplacment v1

---
+++ map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="map" tilewidth="32" tileheight="32" tilecount="63" columns="9">
+ <image source="background.png" width="315" height="250"/>
+</tileset>
