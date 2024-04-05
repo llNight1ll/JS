6bd0426bfb39679735703dbc0e3f565725dd5baf
Your Name <you@example.com>
2024-04-05 09:20:32 +0200

    opacité des collisions

--- component/classe.js
+++ component/classe.js
@@ -54,7 +54,7 @@ class Boundary {
     }
 
     draw() {
-        c.fillStyle = "rgba(255, 0, 0, 10)"
+        c.fillStyle = "rgba(255, 0, 0, 0.5)"
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
         
     }
