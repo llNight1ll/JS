da99d94d42f51eb03c307ecaa18a9fbe7468a9d8
meli <eli.moktar@ynov.com>
2024-04-19 11:13:53 +0200

    modifier les images

--- inventory.js
+++ inventory.js
@@ -1,10 +1,10 @@
 const inventory = {
     enemies: [
-        { name: "Enemy 1", level: 1, health: 100, character: "/path/to/enemy1.png" },
-        { name: "Enemy 2", level: 2, health: 150, character: "/path/to/enemy2.png" },
-        { name: "Enemy 3", level: 3, health: 200, character: "/path/to/enemy3.png" },
-        { name: "Enemy 4", level: 4, health: 250, character: "/path/to/enemy4.png" },
-        { name: "Enemy 5", level: 5, health: 300, character: "/path/to/enemy5.png" }
+        { name: "Enemy 1", level: 1, health: 100, character: "/Survival-Jam-JS/assets/img/ennemy.png" },
+        { name: "Enemy 2", level: 2, health: 150, character: "/Survival-Jam-JS/assets/img/ennemy.png" },
+        { name: "Enemy 3", level: 3, health: 200, character: "/Survival-Jam-JS/assets/img/ennemy.png" },
+        { name: "Enemy 4", level: 4, health: 250, character: "/Survival-Jam-JS/assets/img/ennemy.png" },
+        { name: "Enemy 5", level: 5, health: 300, character: "/Survival-Jam-JS/assets/img/ennemy.png" }
     ],
     
     addEnemy(enemy) {
@@ -25,11 +25,11 @@ const inventory = {
 
 inventory.initBaseEnemies = function() {
     this.enemies = [
-        { name: "Enemy 1", level: 1, health: 100, character: "/path/to/enemy1.png" },
-        { name: "Enemy 2", level: 2, health: 150, character: "/path/to/enemy2.png" },
-        { name: "Enemy 3", level: 3, health: 200, character: "/path/to/enemy3.png" },
-        { name: "Enemy 4", level: 4, health: 250, character: "/path/to/enemy4.png" },
-        { name: "Enemy 5", level: 5, health: 300, character: "/path/to/enemy5.png" }
+        { name: "Enemy 1", level: 1, health: 100, character: "/Survival-Jam-JS/assets/img/ennemy.png" },
+        { name: "Enemy 2", level: 2, health: 150, character: "/Survival-Jam-JS/assets/img/ennemy.png" },
+        { name: "Enemy 3", level: 3, health: 200, character: "/Survival-Jam-JS/assets/img/ennemy.png" },
+        { name: "Enemy 4", level: 4, health: 250, character: "/Survival-Jam-JS/assets/img/ennemy.png" },
+        { name: "Enemy 5", level: 5, health: 300, character: "/Survival-Jam-JS/assets/img/ennemy.png" }
     ];
 };
 
@@ -38,7 +38,7 @@ inventory.initBaseEnemies();
 inventory.showEnemies();
 
 
-const newEnemy = { name: "New Enemy", level: 3, health: 180, character: "/path/to/newenemy.png" };
+const newEnemy = { name: "New Enemy", level: 3, health: 180, character: "/Survival-Jam-JS/assets/img/ennemy.png" };
 inventory.addEnemy(newEnemy);
 
 
